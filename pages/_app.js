42c539e0612c0cee13c1654463d7c058cafc5a8b
bpjatacsuk <bpjatacsuk@instructure.com>
2022-12-01 13:14:31 +0100

    inst ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,37 @@
-import '../styles/globals.css'
+import { InstUISettingsProvider } from "@instructure/emotion";
+import { generateInstanceCounterMap } from "@instructure/ui-react-utils";
+import { canvas } from "@instructure/ui-themes";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+if (!global.Element) {
+  global.Element = function () {};
 }
 
-export default MyApp
+const themeOverrides = {};
+
+function MyApp({ Component, pageProps }) {
+  console.log(canvas)
+  return (
+    <InstUISettingsProvider
+      instanceCounterMap={generateInstanceCounterMap()}
+      theme={canvas}
+    >
+      <InstUISettingsProvider
+        theme={{
+          key: 'Catalog',
+            colors: {
+              
+             backgroundSuccess: 'red',
+             brand: 'orange',
+             backgroundBrand: 'orange',
+             backgroundBrandSecondary: 'red',
+             backgroundLightest: 'orange',
+             },
+        }}
+      >
+        <Component {...pageProps} />
+      </InstUISettingsProvider>
+    </InstUISettingsProvider>
+  );
+}
+
+export default MyApp;
